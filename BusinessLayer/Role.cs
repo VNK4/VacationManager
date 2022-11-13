268aab05b55eff4c99525a9bf0b39920ec69ac9f
vladi0045 <vladi004@abv.bg>
2022-11-13 15:57:19 +0200

    Role fix

--- BusinessLayer/Role.cs
+++ BusinessLayer/Role.cs
@@ -7,23 +7,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BusinessLayer
 {
-    public class Role
+    public enum Role
     {
-        [Key]
-        public int ID { get; private set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        public List<User> User { get; set; }
-
-        public Role()
-        {
-        }
-
-        public Role(string name)
-        {
-            Name = name;
-        }
+        Unassigned,
+        CEO, 
+        Developer, 
+        TeamLead
     }
 }
