bf57c8d7697218f44c1d9a48192ad1ddb20a1cf2
VNK4 <ivan.ignatov2504@gmail.com>
2023-04-03 23:58:22 +0300

    Added image support for Vacations

--- DataLayer/VacationManagerDbContext.cs
+++ DataLayer/VacationManagerDbContext.cs
@@ -29,7 +29,7 @@ namespace DataLayer
             if (!optionsBuilder.IsConfigured)
             {
                 // Replace with your server address
-                optionsBuilder.UseSqlServer(@"Server=[YOURADDRESS];Database=VacationManagerDb;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-HJSDM9L\SQLEXPRESS;Database=VacationManagerDb;Trusted_Connection=True;");
             }
             base.OnConfiguring(optionsBuilder);
         }
--- PresentationLayerWeb/Controllers/VacationsController.cs
+++ PresentationLayerWeb/Controllers/VacationsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -17,31 +18,33 @@ public class VacationsController : Controller
     private readonly VacationManagerDbContext _context;
     private readonly IDB<Vacation, string> _vacationContext;
     private readonly IdentityContext _identityContext;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
     public enum Filter
     {
         Date
     }
-    
+
     public VacationsController(VacationManagerDbContext context, IDB<Vacation, string> vacationContext,
-        IdentityContext identityContext)
+        IdentityContext identityContext, IWebHostEnvironment webHostEnvironment)
     {
         _context = context;
         _vacationContext = vacationContext;
         _identityContext = identityContext;
+        _webHostEnvironment = webHostEnvironment;
 
         _context.Vacations.Include(v => v.Applicant).ToList();
         _context.Users.Include(v => v.Team).ToList();
     }
-    
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public async Task<IActionResult> Index(string searchString, int filter)
     {
         var vacations = await _vacationContext.ReadAllAsync();
         if (User.IsInRole(Role.CEO.ToString()))
         {
             ViewData["Title"] = "Vacations - CEO";
-            
+
             if (!string.IsNullOrEmpty(searchString))
             {
                 vacations = filter switch
@@ -81,38 +84,47 @@ public class VacationsController : Controller
 
         return View(vacations);
     }
-    
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public IActionResult Create()
     {
         ViewData["ApplicantID"] = new SelectList(_context.Users, "Id", "UserName");
-        return View();
+        return View(new VacationViewModel());
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public async Task<IActionResult> Create(
-        [Bind("BeginningDate,EndDate,VacationType")] VacationViewModel vacationViewModel)
+        [Bind("BeginningDate,EndDate,VacationType,VacationImage")]
+        VacationViewModel vacationViewModel)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid)
+            return View(vacationViewModel);
+
+        var beginningDate = vacationViewModel.BeginningDate;
+        var endDate = vacationViewModel.EndDate;
+        var vacationType = vacationViewModel.VacationType;
+        var img = vacationViewModel.VacationImage;
+        var user = await _identityContext.FindUserByNameAsync(User.Identity.Name);
+        var filePath = string.Empty;
+        if (img != null)
         {
-            var beginningDate = vacationViewModel.BeginningDate;
-            var endDate = vacationViewModel.EndDate;
-            var vacationType = vacationViewModel.VacationType;
-            var user = await _identityContext.FindUserByNameAsync(User.Identity.Name);
-
-            var vacation = new Vacation(beginningDate, endDate, vacationType, user);
-
-            await _vacationContext.CreateAsync(vacation);
-
-            return RedirectToAction(nameof(Index));
+            var uniqueFileName = CreateUniqueFileName(img.FileName);
+            var uploads = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+            filePath = Path.Combine(uploads, uniqueFileName);
+            await vacationViewModel.VacationImage.CopyToAsync(new FileStream(filePath, FileMode.Create));
+            filePath = uniqueFileName;
         }
 
-        return View(vacationViewModel);
+        var vacation = new Vacation(beginningDate, endDate, vacationType, user, filePath);
+
+        await _vacationContext.CreateAsync(vacation);
+
+        return RedirectToAction(nameof(Index));
     }
-    
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public async Task<IActionResult> Info(string id)
     {
         if (id == null || _context.Vacations == null)
@@ -132,9 +144,9 @@ public class VacationsController : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public async Task<IActionResult> Info(string id,
-        [Bind("BeginningDate,EndDate,VacationType")]
+        [Bind("BeginningDate,EndDate,VacationType,VacationImage")]
         VacationViewModel vacationViewModel)
     {
         if (ModelState.IsValid)
@@ -146,15 +158,32 @@ public class VacationsController : Controller
             vacation.HalfDayVacation = false;
             vacation.Accepted = false;
             vacation.VacationType = vacationViewModel.VacationType;
-
+            
+            var img = vacationViewModel.VacationImage;
+            var filePath = string.Empty;
+            if (img != null)
+            {
+                var uniqueFileName = CreateUniqueFileName(img.FileName);
+                if(!Directory.Exists(Path.Combine(_webHostEnvironment.WebRootPath, "images")))
+                {
+                    Directory.CreateDirectory(Path.Combine(_webHostEnvironment.WebRootPath, "images"));
+                }
+                var uploads = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                filePath = Path.Combine(uploads, uniqueFileName);
+                await vacationViewModel.VacationImage.CopyToAsync(new FileStream(filePath, FileMode.Create));
+                filePath = uniqueFileName;
+                System.IO.File.Delete(Path.Combine(_webHostEnvironment.WebRootPath, "images", vacation.ImageFilePath));
+                vacation.ImageFilePath = filePath;
+            }
+            
             await _vacationContext.UpdateAsync(vacation);
             return RedirectToAction(nameof(Index));
         }
 
         return View(vacationViewModel);
     }
-    
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public async Task<IActionResult> Delete(string id)
     {
         if (id == null || _context.Vacations == null)
@@ -171,10 +200,10 @@ public class VacationsController : Controller
 
         return View(vacation);
     }
-    
+
     [HttpPost, ActionName("Delete")]
     [ValidateAntiForgeryToken]
-    [Authorize(Roles="Unassigned,Developer,CEO,TeamLead")]
+    [Authorize(Roles = "Unassigned,Developer,CEO,TeamLead")]
     public async Task<IActionResult> DeleteConfirmed(string id)
     {
         if (_context.Vacations == null)
@@ -186,12 +215,17 @@ public class VacationsController : Controller
 
         if (vacation != null)
         {
+            if (vacation.ImageFilePath != string.Empty)
+            {
+                var filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", vacation.ImageFilePath);
+                System.IO.File.Delete(Path.Combine(_webHostEnvironment.WebRootPath, "images", vacation.ImageFilePath));
+            }
             await _vacationContext.DeleteAsync(vacation.Id);
         }
 
         return RedirectToAction(nameof(Index));
     }
-    
+
     [Authorize(Roles = "CEO,TeamLead")]
     public async Task<IActionResult> Approve(string id)
     {
@@ -220,7 +254,7 @@ public class VacationsController : Controller
         if (ModelState.IsValid)
         {
             var vacation = await _vacationContext.ReadAsync(id);
-            
+
             vacation.HalfDayVacation = vacationViewModel.HalfDayVacation;
             vacation.Accepted = true;
 
@@ -230,7 +264,7 @@ public class VacationsController : Controller
 
         return View(vacationViewModel);
     }
-    
+
     [Authorize(Roles = "CEO,TeamLead")]
     public async Task<IActionResult> Reject(string id)
     {
@@ -248,7 +282,7 @@ public class VacationsController : Controller
 
         return View(vacation);
     }
-    
+
     [HttpPost, ActionName("Reject")]
     [ValidateAntiForgeryToken]
     [Authorize(Roles = "CEO,TeamLead")]
@@ -263,13 +297,26 @@ public class VacationsController : Controller
 
         if (vacation != null)
         {
+            if (vacation.ImageFilePath != string.Empty)
+            {
+                var filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", vacation.ImageFilePath);
+                System.IO.File.Delete(filePath);
+            }
             await _vacationContext.DeleteAsync(vacation.Id);
         }
 
         return RedirectToAction(nameof(Index));
     }
 
-    
+    private static string CreateUniqueFileName(string fileName)
+    {
+        fileName = Path.GetFileName(fileName);
+        return string.Concat(Path.GetFileNameWithoutExtension(fileName),
+            "_",
+            Guid.NewGuid().ToString().AsSpan(0, 4),
+            Path.GetExtension(fileName));
+    }
+
     [NonAction]
     private static VacationViewModel ConvertToViewModel(Vacation vacation)
     {
@@ -282,7 +329,8 @@ public class VacationsController : Controller
             HalfDayVacation = vacation.HalfDayVacation,
             Accepted = vacation.Accepted,
             VacationType = vacation.VacationType,
-            Applicant = vacation.Applicant
+            Applicant = vacation.Applicant,
+            ImageFilePath = vacation.ImageFilePath
         };
     }
 }
